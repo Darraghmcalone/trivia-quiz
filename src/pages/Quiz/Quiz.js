ba1912be92f030f72ac4803874fb6b0b43b842d0
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-01-10 16:13:33 +0000

    Add logic to change questions after answering a question.

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -15,10 +15,30 @@ export default class Quiz extends Component {
     async componentDidMount() {
         try {
             const questions = await loadQuestions();
-            this.setState({ questions, currentQuestion: questions[0], loading: false });
+            this.setState(
+                { questions },
+                () => {
+                    this.changeQuestion();
+                }
+            );
         } catch (err) {
             console.error(err);
         }
+    }
+    changeQuestion = () => {
+        // get a random index of a question
+        const getRandomQuestionIndex = Math.floor(Math.random() * this.state.questions.length);
+        // Set the current question to the question at the random question index
+        const currentQuestion = this.state.questions[getRandomQuestionIndex]
+        // Remove that question from the questions going forward
+        const remainingQuestions = [...this.state.questions]
+        remainingQuestions.splice(getRandomQuestionIndex, 1)
+        //Update the state to reflect these changes
+        this.setState({
+            questions: remainingQuestions,
+            currentQuestion,
+            loading: false
+        })
     }
 
     render() {
