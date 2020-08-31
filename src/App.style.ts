744e2b5159a182a7ef514591804a7522c3d8e014
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-08-31 15:17:05 +0100

    Update styling

--- src/App.style.ts
+++ src/App.style.ts
@@ -9,7 +9,7 @@ align-items: center;
 width: 65vw;
 max-width: 80rem;
 margin: 0 auto;
-padding: 2rem;
+padding: 0 2rem;
 background: linear-gradient(rgba(250,0,0,0.5),transparent);
 background-color: white;
 @media only screen and (max-width: 786px) {
