d7cf86ce0e7665b5655db8b240c2097c5431f81a
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-08-31 19:40:19 +0100

    Update Home Page styling

--- src/pages/Home/Home.styled.ts
+++ src/pages/Home/Home.styled.ts
@@ -2,20 +2,20 @@ import styled from 'styled-components';
 import { Button } from '../../components/Button/Button'
 
 export const HomeContainer = styled.div`
-height: 35%;
+height: 53%;
 flex-direction: column;
 display: flex;
 align-items: center;
 justify-content: space-between;
 h1 {
-    font-size: 4em;
+    font-size: 6em;
     color: #56a5eb;
 }
-@media only screen and (max-width: 786px) {
-    h1 {
-        font-size: 3.5em;
-    }
-}
+@media only screen and (max-width: 425px) {
+h1 {
+    font-size: 3.5em;
+ }
+}  
 `;
 
 
@@ -23,4 +23,19 @@ export const HomeButton = styled(Button)`
 @media only screen and (max-width: 786px) {
 width: 70%;
 }
+@media only screen and (min-width: 426px) {
+font-size: 2.5em;
+}
 `;
+
+export const ButtonContainer = styled.div`
+display: flex;
+flex-direction: column;
+height: 50%;
+width: 90%;
+justify-content: space-evenly;
+align-items: center;
+@media only screen and (min-width: 786px) {
+width: 60%;
+}
+`
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { HomeContainer, HomeButton } from './Home.styled'
+import { HomeContainer, HomeButton, ButtonContainer } from './Home.styled'
 
 export default function Home() {
     return (
         <HomeContainer>
             <h1>Trivia Quiz</h1>
-            <HomeButton as={Link} to="/quiz">Start Quiz</HomeButton>
-            <HomeButton as={Link} to="/highscores">High Scores</HomeButton>
+            <ButtonContainer>
+                <HomeButton as={Link} to="/quiz">Start Quiz</HomeButton>
+                <HomeButton as={Link} to="/highscores">High Scores</HomeButton>
+            </ButtonContainer>
         </HomeContainer>
 
     )
