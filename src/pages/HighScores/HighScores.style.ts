8353eb2e2483e7ebcd5bc10ba5e9536090480e39
Darragh Mcalone <darraghmcalone@Darraghs-MacBook-Pro.local>
2022-10-19 15:02:51 +0800

    Update high scores styling

--- src/pages/HighScores/HighScores.style.ts
+++ src/pages/HighScores/HighScores.style.ts
@@ -1,43 +1,77 @@
 import styled from 'styled-components';
-import { Button } from "../../components/Button/Button";
+import { Button } from '../../components/Button/Button';
 
 export const HighScoreContainer = styled.div`
-height: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+  height: 100%;
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 426px) {
+    width: 100%;
+  }
 `;
 
 export const HighScoresList = styled.div`
-list-style: none;
-padding-left: 0;
-margin-bottom: 4rem;
-li {
-    font-size: 2.8rem;
-    margin-bottom: 0.5rem;
-}
-@media only screen and (max-width: 768px) {
-    width: 70%;
-    li { font-size: 2rem;}
-}
-@media only screen and (min-width: 768px) {
+  list-style: none;
+  padding-left: 0;
+  margin-bottom: 4rem;
+  text-align: left;
+  div {
+    font-size: 1.75rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
     width: 100%;
-}
+  }
+  .leftCol {
+    width: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    h5 {
+      margin-left: 40px;
+    }
+  }
+  .highScoreItem {
+    display: flex;
+    align-items: center;
+    border-radius: 6px;
+    border: none;
+    box-shadow: 0 0rem 0.4rem 0 rgb(86 185 235 / 50%);
+    padding: 0px 15px;
+    margin-top: 25px;
+    height: 70px;
+  }
+  h5,h6 {
+    margin: 0;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 70%;
+    div {
+      font-size: 1.5rem;
+    }
+  }
+  @media only screen and (min-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const HighScoreTitle = styled.h1`
-font-size: 5.41rem;
-color: #56a5eb;
-margin-bottom: 5rem;
-@media only screen and (max-width: 768px) {
-    margin-bottom: 3rem;
-    font-size: 3rem;
-} 
-`
+  font-size: 2.5rem;
+  color: #56a5eb;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
 export const HighScoreButton = styled(Button)`
-width: 70%;
-@media only screen and (min-width: 768px) {
+  width: 70%;
+  margin-bottom: 5rem;
+  @media only screen and (min-width: 768px) {
     width: 50%;
-}
-`
+  }
+`;
--- src/pages/HighScores/HighScores.tsx
+++ src/pages/HighScores/HighScores.tsx
@@ -1,69 +1,80 @@
-import React, { useState, useEffect, FunctionComponent } from 'react'
+import React, { useState, useEffect, FunctionComponent } from 'react';
 import { useFirebase } from '../../components/Firebase/FirebaseContext';
 import { Loader } from '../../components/Loader/Loader';
-import { HighScoreContainer, HighScoresList, HighScoreTitle, HighScoreButton } from './HighScores.style'
+import {
+  HighScoreContainer,
+  HighScoresList,
+  HighScoreTitle,
+  HighScoreButton,
+} from './HighScores.style';
 import { Link } from 'react-router-dom';
 
 interface Score {
-    key: string;
-    name: string;
-    score: number;
+  key: string;
+  name: string;
+  score: number;
 }
 
 interface HighScoresProps {
-    highScores: Score[];
-    loadingInit?: boolean;   
+  highScores: Score[];
+  loadingInit?: boolean;
 }
 
- const HighScores: FunctionComponent <HighScoresProps> = ({highScores, loadingInit = true}) =>  {
-   
-    const firebase = useFirebase();
-    const [scores, setScores] = useState(highScores);
-    const [loading, setLoading] = useState(loadingInit);
+const HighScores: FunctionComponent<HighScoresProps> = ({
+  highScores,
+  loadingInit = true,
+}) => {
+  const firebase = useFirebase();
+  const [scores, setScores] = useState(highScores);
+  const [loading, setLoading] = useState(loadingInit);
 
-    useEffect(() => {
-        firebase.scores().once('value', (snapshot: any) => {
-            const data = snapshot.val();
-            const sortedScores = formatScoreData(data);
-            setScores(sortedScores);
-            setLoading(false);
-        });
-    },[firebase]);
+  useEffect(() => {
+    firebase.scores().once('value', (snapshot: any) => {
+      const data = snapshot.val();
+      const sortedScores = formatScoreData(data);
+      setScores(sortedScores);
+      setLoading(false);
+    });
+  }, [firebase]);
 
-    const formatScoreData = (firebaseScores: any) => {
-        const scores = [];
+  const formatScoreData = (firebaseScores: any) => {
+    const scores = [];
 
-        for (let key in firebaseScores) {
-            const val = firebaseScores[key];
-            val['key'] = key;
-            scores.push(val);
-        }
+    for (let key in firebaseScores) {
+      const val = firebaseScores[key];
+      val['key'] = key;
+      scores.push(val);
+    }
 
-        return scores
-            .sort((score1, score2) => score2.score - score1.score)
-            .slice(0, 10);
-    };
+    return scores
+      .sort((score1, score2) => score2.score - score1.score)
+      .slice(0, 10);
+  };
 
-    return (
-        <>
-            {loading && <Loader />}
-            {!loading && (
-                <HighScoreContainer>
-                    <HighScoreTitle>High Scores</HighScoreTitle>
-                    <HighScoresList>
-                        {scores.map((record) => (                         
-                            <li key={record.key}>
-                                {record.name} - {record.score}
-                            </li>
-                        ))}
-                    </HighScoresList>
-                    <HighScoreButton as={Link} to="/" >
-                        Go Home
-                    </HighScoreButton>
-                </HighScoreContainer>
-            )}
-        </>
-    );
-}
+  return (
+    <>
+      {loading && <Loader />}
+      {!loading && (
+        <HighScoreContainer>
+          <HighScoreTitle>High Scores</HighScoreTitle>
+          <HighScoresList>
+            {scores.map((record, index) => (
+              <div className="highScoreItem" key={record.key}>
+                <div className="leftCol">
+                  <h6> {index + 1} </h6>
+                  <h5>{record.name}</h5>
+                </div>
+                <h5>{record.score}</h5>
+              </div>
+            ))}
+          </HighScoresList>
+          <HighScoreButton as={Link} to='/'>
+            Go Home
+          </HighScoreButton>
+        </HighScoreContainer>
+      )}
+    </>
+  );
+};
 
-export default HighScores
+export default HighScores;
