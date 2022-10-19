71440f1b2a3719f1c38241810a2989d5aa82c64f
Darragh Mcalone <darraghmcalone@Darraghs-MacBook-Pro.local>
2022-10-19 17:33:05 +0800

    Update quiz styling

--- src/pages/Quiz/Quiz.style.ts
+++ src/pages/Quiz/Quiz.style.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const QuizContainer = styled.div`
-height: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 
