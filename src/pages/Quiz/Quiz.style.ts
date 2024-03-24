08e397c7275b7a7d19ed9e9f9a131a612bd47c24
Darragh Mcalone <darraghmcalone@Darraghs-MacBook-Pro.local>
2024-03-24 22:07:09 +0000

    Update styling

--- src/pages/Quiz/Quiz.style.ts
+++ src/pages/Quiz/Quiz.style.ts
@@ -1,18 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  min-height: 100vh;
+  min-height: 100dvh;
   justify-content: flex-start;
 
 `;
 
 export const QuizContainer = styled.div`
-  min-height: 100vh;
+  min-height: 100dvh;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
-  padding-top: 1rem;
   h3 {
     color: ${(props) => props.theme.colors.darkGrey};
   }
